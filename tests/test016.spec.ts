78a9240b3be4fc698dc011bf692bc4d1b3df878d
SamSonant <sam@sonant.com>
2026-02-11 13:48:44 -0800

    created test016 test017

---
+++ tests/test016.spec.ts
@@ -0,0 +1,11 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.goto('https://qa3customer.sonant.com/MNPaymentApplication/');
+  await page.locator('#CaseNumber').click();
+  await page.locator('#CaseNumber').fill('25JV253');
+  await page.getByRole('button', { name: 'Find cases by case number' }).click();
+  await page.getByRole('radio', { name: 'Goodhue County Health and' }).check();
+  await page.getByRole('button', { name: 'Continue with selected party' }).click();
+  await expect(page.getByLabel('Cancel and return to previous')).toContainText('Search Again');
+});
