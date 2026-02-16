674efe35c72fe50f72d3a7f1549c39525346dffa
SamSonant <sam@sonant.com>
2026-02-15 22:58:27 -0800

    created test042

---
+++ tests/test042.spec.ts
@@ -0,0 +1,14 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.goto('https://qa3customer.sonant.com/MNPaymentApplication/');
+  await page.getByRole('radio', { name: 'Citation Number' }).check();
+  await page.locator('#CitationNumber').click();
+  await page.locator('#CitationNumber').fill('32820250810');
+  await page.getByRole('button', { name: 'Find cases by citation number' }).click();
+  
+  let locator = page.locator('p').filter({ hasText: 'If your name is not listed or' });
+  await expect(locator).toHaveClass(/sn-note-important/);
+  let checkColor = await locator.evaluate(el => window.getComputedStyle(el).color);
+  await expect(checkColor).toBe('rgb(151, 108, 24)');
+ });
