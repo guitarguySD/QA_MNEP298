030c8e0556202ecf25c117716304b3a44b4e1af0
SamSonant <sam@sonant.com>
2026-02-15 21:15:53 -0800

    created test032 test035

---
+++ tests/test032.spec.ts
@@ -0,0 +1,15 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.goto('https://qa3customer.sonant.com/MNPaymentApplication/');
+  await page.locator('#CaseNumber').click();
+  await page.locator('#CaseNumber').fill('25jv253');
+  await page.getByRole('button', { name: 'Find cases by case number' }).click();
+  await page.getByRole('row', { name: 'Name Not Available Name Not Available, Amount owed: $55.00 $' }).getByLabel('Name Not Available').check();
+  await page.getByRole('button', { name: 'Continue with selected party' }).click();
+  await page.getByRole('spinbutton', { name: 'Make a Payment of' }).click();
+  await page.getByRole('spinbutton', { name: 'Make a Payment of' }).fill('1');
+  await page.locator('div').filter({ hasText: 'Case Details Case Number: 25-' }).nth(2).click();
+  await expect(page.getByRole('spinbutton', { name: 'Make a Payment of' })).toHaveValue('1.00');
+  await page.getByRole('button', { name: 'Pay now for this case' }).click();
+});
---
+++ tests/test035.spec.ts
@@ -0,0 +1,22 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.goto('https://qa3customer.sonant.com/MNPaymentApplication/');
+  await page.locator('#CaseNumber').click();
+  await page.locator('#CaseNumber').fill('25jv253');
+  await page.getByRole('button', { name: 'Find cases by case number' }).click();
+  await page.getByRole('row', { name: 'Name Not Available Name Not Available, Amount owed: $249.00 $' }).getByLabel('Name Not Available').check();
+  await page.getByRole('button', { name: 'Continue with selected party' }).click();
+  await expect(page.getByLabel('Make Payment')).toMatchAriaSnapshot(`
+    - listitem:
+      - text: If you have been charged with
+      - strong: No Insurance
+      - text: or
+      - strong: No Proof of Insurance
+      - text: and you can provide proof of insurance, do not pay at this time.
+      - list:
+        - listitem: If the vehicle you were driving was insured at that time or if you are not the owner of the vehicle you were driving and can provide the name and address of the owner, you may be able to have this charge dismissed by providing proof of insurance coverage,
+        - listitem: Proof of insurance or the name and address of the owner of the vehicle must be provided to court administration no later than the date and time of your first court appearance.
+        - listitem: Proof must include the insurance card, name of insurance company, policy number, insurance agent name and phone number, and effective and ending dates of coverage. Be sure to include your case or citation number with proof of insurance.
+    `);
+});
