7fcad978f27847689673df9c31d5f73830382c49
SamSonant <sam@sonant.com>
2026-02-16 09:53:54 -0800

    fixed test017 test028

--- tests/test017.spec.ts
+++ tests/test017.spec.ts
@@ -1,10 +1,12 @@
 import { test, expect } from '@playwright/test';
 
 test('should display Case Details heading after selecting a party', async ({ page }) => {
-  await page.goto('https://qa3customer.sonant.com/MNPaymentApplication');
+ await page.goto('https://qa3customer.sonant.com/MNPaymentApplication/');
+  await page.getByRole('radio', { name: 'Case Number' }).check();
   await page.locator('#CaseNumber').click();
-  await page.locator('#CaseNumber').fill('25JV253');
+  await page.locator('#CaseNumber').fill('25jv253');
   await page.getByRole('button', { name: 'Find cases by case number' }).click();
+  await page.getByRole('row', { name: 'Name Not Available Name Not Available, Amount owed: $250.00 $' }).getByLabel('Name Not Available').check();
   await page.getByRole('button', { name: 'Continue with selected party' }).click();
   await expect(page.locator('h1')).toContainText('Case Details');
 });
--- tests/test028.spec.ts
+++ tests/test028.spec.ts
@@ -2,14 +2,20 @@ import { test, expect } from '@playwright/test';
 
 test('should display no charges available message for both parties', async ({ page }) => {
   await page.goto('https://qa3customer.sonant.com/MNPaymentApplication/');
+  await page.getByRole('radio', { name: 'Case Number' }).check();
   await page.locator('#CaseNumber').click();
   await page.locator('#CaseNumber').fill('25FA2514');
   await page.getByRole('button', { name: 'Find cases by case number' }).click();
-  await page.goto('https://qa3customer.sonant.com/MNPaymentApplication/Case/PartySelection');
+  await page.getByRole('radio', { name: 'Testing, Petitioner' }).check();
   await page.getByRole('button', { name: 'Continue with selected party' }).click();
   await expect(page.locator('#main-content')).toContainText('No charges available');
-  await page.goto('https://qa3customer.sonant.com/MNPaymentApplication/Case/PartySelection');
-  await page.getByRole('radio', { name: 'Testing, Petitioner' }).check();
+  await page.getByRole('button', { name: 'Cancel and return to previous' }).click();
+  await page.locator('#CaseNumber').click();
+  await page.getByRole('radio', { name: 'Case Number' }).check();
+  await page.locator('#CaseNumber').click();
+  await page.locator('#CaseNumber').fill('25FA2514');
+  await page.getByRole('button', { name: 'Find cases by case number' }).click();
+  await page.getByRole('radio', { name: 'Testing, Respondent' }).check();
   await page.getByRole('button', { name: 'Continue with selected party' }).click();
   await expect(page.locator('#main-content')).toContainText('No charges available');
 });
