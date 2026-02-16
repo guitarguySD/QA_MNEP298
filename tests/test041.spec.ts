32d11c91b66abd27c5557e6f32e0d7d3126480bc
SamSonant <sam@sonant.com>
2026-02-15 22:12:42 -0800

    create test041

---
+++ tests/test041.spec.ts
@@ -0,0 +1,11 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.goto('https://qa3customer.sonant.com/MNPaymentApplication/');
+  await page.getByRole('radio', { name: 'Citation Number' }).check();
+  await page.locator('#CitationNumber').click();
+  await page.locator('#CitationNumber').fill('32820250810');
+  await page.getByRole('button', { name: 'Find cases by citation number' }).click();
+  await page.goto('https://qa3customer.sonant.com/MNPaymentApplication/Case/PartySelection');
+  await expect(page.locator('#main-content')).toContainText('The case you selected has multiple parties. Please choose the party related to your case. If unsure, refer to your court notice.');
+});
