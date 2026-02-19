b2d2800c3111d0e6c33313fd22c4f680a61bcffc
SamSonant <sam@sonant.com>
2026-02-19 09:26:09 -0800

    fixed test008

--- tests/test008.spec.ts
+++ tests/test008.spec.ts
@@ -1,16 +1,12 @@
 import { test, expect } from '@playwright/test';
 
-test('should display driver\'s license form after returning from case search', async ({ page }) => {
+test('test', async ({ page }) => {
   await page.goto('https://qa3customer.sonant.com/MNPaymentApplication/');
-  await page.getByRole('radio', { name: 'Driver\'s License Number' }).check();
-  await page.locator('#DriversLicenseNumber').click();
-  await page.getByTitle('Enter your case number').click();
-  await page.locator('#CaseNumber').click();
-  await page.locator('#CaseNumber').click();
-  await page.locator('#CaseNumber').fill('25jv253');
+  await page.getByRole('radio', { name: 'Case Number' }).check();
+  await page.getByRole('textbox', { name: 'Enter Case Number' }).click();
+  await page.getByRole('textbox', { name: 'Enter Case Number' }).fill('25jv253');
   await page.getByRole('button', { name: 'Find cases by case number' }).click();
-  await page.goto('https://qa3customer.sonant.com/MNPaymentApplication/Case/PartySelection');
-  await page.getByRole('button', { name: 'Return to search page' }).click();
+  await page.getByRole('link', { name: 'Search Again' }).click();
   await page.getByRole('radio', { name: 'Driver\'s License Number' }).check();
   await expect(page.locator('#driversLicenseForm')).toContainText('Enter Driver\'s License Number');
 });
