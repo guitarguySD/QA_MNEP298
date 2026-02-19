8236daa5dc876562c00cc04948dc8a61657078a7
SamSonant <sam@sonant.com>
2026-02-19 09:34:53 -0800

    fixed test017

--- tests/test017.spec.ts
+++ tests/test017.spec.ts
@@ -1,12 +1,13 @@
 import { test, expect } from '@playwright/test';
 
 test('should display Case Details heading after selecting a party', async ({ page }) => {
- await page.goto('https://qa3customer.sonant.com/MNPaymentApplication/');
+  await page.goto('https://qa3customer.sonant.com/MNPaymentApplication/');
   await page.getByRole('radio', { name: 'Case Number' }).check();
-  await page.locator('#CaseNumber').click();
-  await page.locator('#CaseNumber').fill('25jv253');
+  await page.getByRole('textbox', { name: 'Enter Case Number' }).click();
+  await page.getByRole('textbox', { name: 'Enter Case Number' }).fill('25jv253');
   await page.getByRole('button', { name: 'Find cases by case number' }).click();
-  await page.getByRole('row', { name: 'Name Not Available Name Not Available, Amount owed: $250.00 $' }).getByLabel('Name Not Available').check();
+  await page.getByRole('row', { name: 'Name Not Displayed Name Not Displayed, Amount owed: $250.00 $' }).getByLabel('Name Not Displayed').check();
+  await page.getByRole('radio', { name: 'Goodhue County Health and' }).check();
   await page.getByRole('button', { name: 'Continue with selected party' }).click();
   await expect(page.locator('h1')).toContainText('Case Details');
 });
