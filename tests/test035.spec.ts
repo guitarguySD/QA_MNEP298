ae305636c152f712eb5285dea4f9906982801211
SamSonant <sam@sonant.com>
2026-02-24 09:46:18 -0800

    fixed test035

--- tests/test035.spec.ts
+++ tests/test035.spec.ts
@@ -2,21 +2,20 @@ import { test, expect } from '@playwright/test';
 
 test('should display insurance proof warning message for no insurance charge', async ({ page }) => {
   await page.goto('https://qa3customer.sonant.com/MNPaymentApplication/');
-  await page.locator('#CaseNumber').click();
-  await page.locator('#CaseNumber').fill('25jv253');
+  await page.getByRole('radio', { name: 'Case Number' }).check();
+  await page.getByRole('textbox', { name: 'Enter Case Number' }).click();
+  await page.getByRole('textbox', { name: 'Enter Case Number' }).fill('25jv253');
   await page.getByRole('button', { name: 'Find cases by case number' }).click();
-  await page.getByRole('row', { name: 'Name Not Available Name Not Available, Amount owed: $249.00 $' }).getByLabel('Name Not Available').check();
+  await page.getByRole('row', { name: 'Name Not Displayed Name Not Displayed, Amount owed: $250.00 $' }).getByLabel('Name Not Displayed').check();
   await page.getByRole('button', { name: 'Continue with selected party' }).click();
   await expect(page.getByLabel('Make Payment')).toMatchAriaSnapshot(`
     - listitem:
-      - text: If you have been charged with
-      - strong: No Insurance
-      - text: or
-      - strong: No Proof of Insurance
-      - text: and you can provide proof of insurance, do not pay at this time.
-      - list:
-        - listitem: If the vehicle you were driving was insured at that time or if you are not the owner of the vehicle you were driving and can provide the name and address of the owner, you may be able to have this charge dismissed by providing proof of insurance coverage,
-        - listitem: Proof of insurance or the name and address of the owner of the vehicle must be provided to court administration no later than the date and time of your first court appearance.
-        - listitem: Proof must include the insurance card, name of insurance company, policy number, insurance agent name and phone number, and effective and ending dates of coverage. Be sure to include your case or citation number with proof of insurance.
+      - text: If no other offense on this case or citation requires a
+      - strong: mandatory court appearance
+      - text: ", and you can provide proof of insurance, do not pay this citation at this time."
+      - link "Contact the Court Payment Center (opens in new window)":
+        - /url: https://mncourts.gov/Pay-a-Fine.aspx
+        - text: ""
+      - text: for instructions and information for paying other offenses on the citation.
     `);
 });
