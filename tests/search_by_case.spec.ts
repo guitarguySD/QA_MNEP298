e03892d7fecaf56b194732615a3e73c42df7338b
SamSonant <sam@sonant.com>
2026-02-26 11:46:50 -0800

    created search_by_case

---
+++ tests/search_by_case.spec.ts
@@ -0,0 +1,26 @@
+import { test, expect } from '@playwright/test';
+
+test('Search by case number. Adult criminal, juvenile criminal, non-criminal', async ({ page }) => {
+  await page.goto('https://qa3customer.sonant.com/MNPaymentApplication/');
+  await page.getByRole('radio', { name: 'Case Number' }).check();
+  await page.getByRole('textbox', { name: 'Case Number' }).click();
+  await page.getByRole('textbox', { name: 'Case Number' }).fill('70vb2539');
+  await page.getByRole('button', { name: 'Find cases by case number' }).click();
+  await page.goto('https://qa3customer.sonant.com/MNPaymentApplication/Case/CaseDetail?caseId=1629859308&payorId=1621404135');
+  await expect(page.locator('#main-content')).toContainText('Crim/Traf Non-Mand');
+  await page.getByRole('link', { name: 'Search Again' }).click();
+  await page.getByRole('radio', { name: 'Case Number' }).check();
+  await page.getByRole('textbox', { name: 'Case Number' }).click();
+  await page.getByRole('textbox', { name: 'Case Number' }).fill('25jv254');
+  await page.getByRole('button', { name: 'Find cases by case number' }).click();
+  await expect(page.locator('#main-content')).toContainText('Juvenile Petty Offense');
+  await page.getByRole('link', { name: 'Search Again' }).click();
+  await page.getByRole('textbox', { name: 'Case Number' }).click();
+  await page.getByRole('radio', { name: 'Case Number' }).check();
+  await page.getByRole('textbox', { name: 'Case Number' }).click();
+  await page.getByRole('textbox', { name: 'Case Number' }).fill('70cv2540');
+  await page.getByRole('button', { name: 'Find cases by case number' }).click();
+  await page.getByRole('radio', { name: 'testing, defendant' }).check();
+  await page.getByRole('button', { name: 'Continue with selected party' }).click();
+  await expect(page.locator('#main-content')).toContainText('Property Damage');
+});
