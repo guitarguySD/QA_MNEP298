b4237d1bd10ae60661bd9203bfe03c0f991e7766
SamSonant <sam@sonant.com>
2026-02-26 13:14:22 -0800

    fixed test003

--- tests/test003.spec.ts
+++ tests/test003.spec.ts
@@ -1,10 +1,7 @@
 import { test, expect } from '@playwright/test';
 
 test('should display MN Court Payment Center heading and Online Payment System text', async ({ page }) => {
-  await page.goto('https://qa3customer.sonant.com/mnpaymentapplication');
-  const heading = page.getByRole('heading', { name: 'MN Court Payment Center' });
-
-  
-  await expect(heading).toContainText('MN Court Payment Center');
-  await expect(page.locator('body')).toContainText('Online Payment System');
+  await page.goto('https://qa3customer.sonant.com/MNPaymentApplication/');
+  await expect(page.getByRole('banner')).toContainText('MN Court Payment CenterOnline Payment System');
+  await expect(page.getByRole('banner')).toContainText('MN Court Payment CenterOnline Payment System 0');
 });
